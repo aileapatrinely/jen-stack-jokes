aca6279281d255af8602ba770f5e966e88bb4793
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-06 13:35:22 -0500

    clicking button adds joke object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,21 @@
 console.log('client.js sourced');
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
+
+const joke = {};
 
 function onReady() {
-    console.log('DOM ready');
+  console.log('DOM ready');
+  $('#addJokeButton').on('click', addJoke);
 }
+
+function addJoke() {
+  joke.whoseJoke = $('#whoseJokeIn').val();
+  joke.jokeQuestion = $('#questionIn').val();
+  joke.punchLine = $('#punchlineIn').val();
+  console.log(joke);
+
+  postJoke()
+}
+
+function
